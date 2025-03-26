5d13df359ee3f69f6144877bebceabaf37d25d8b
AngelicaZ <cyzhang@yeah.net>
2025-03-26 15:24:54 +0000

    replace all the values that has underline with space

--- replace_underline.py
+++ replace_underline.py
@@ -3,13 +3,14 @@ import numpy as np
 
 def replace_underline():
     # Read the CSV file
-    df = pd.read_csv('./experiment_summary.csv')
+    df = pd.read_csv('./summary_old.csv')
     
-    # Replace underline with space
-    df.columns = df.columns.str.replace('_', ' ')
+    # Replace underline with space in all string columns (data values only)
+    for col in df.select_dtypes(include=['object']).columns:
+        df[col] = df[col].str.replace('_', ' ')
     
     # Save results
-    df.to_csv('experiment_summary_with_space.csv')
+    df.to_csv('summary_old_with_space.csv')
 
 if __name__ == "__main__":
     replace_underline()
