bfa95de81999471103370c06db3baaa23f3ea985
Daniil Cherniavskii <danchern97@mail.ru>
2025-03-28 11:42:33 +0100

    Processed mp4 to webm

--- convert_to_webm.py
+++ convert_to_webm.py
@@ -3,10 +3,20 @@
 
 import os
 import sys
+import argparse
 
 def convert_to_webm(video_path):
     # Convert the video to webm format
     os.system(f"ffmpeg -i {video_path} -c:v libvpx-vp9 -crf 30 -b:v 0 -c:a libopus -b:a 128k {video_path.replace('.mp4', '.webm')}")
 
 if __name__ == "__main__":
-    convert_to_webm(sys.argv[1])
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--videos_path", type=str, required=True)
+    args = parser.parse_args()
+
+    # Recursively find all videos in the directory
+    for root, dirs, files in os.walk(args.videos_path):
+        for file in files:
+            if file.endswith(".mp4"):
+                print(f"Converting {os.path.join(root, file)} to webm")
+                convert_to_webm(os.path.join(root, file))
